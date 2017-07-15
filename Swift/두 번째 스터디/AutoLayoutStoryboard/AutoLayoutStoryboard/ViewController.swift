358eb5e8066ff830a2d3c679f56a15e4824bbf0e
Daeyun Ethan Kim <nicebuvr@naver.com>
2017-07-15 21:45:25 +0900

    [두 번째 스터디] 스토리보드로 오토레이아웃 작

---
+++ Swift/두 번째 스터디/AutoLayoutStoryboard/AutoLayoutStoryboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutStoryboard
+//
+//  Created by Daeyun Ethan Kim on 12/07/2017.
+//  Copyright © 2017 Daeyun Ethan Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Swift/두 번째 스터디/README.md
@@ -0,0 +1,5 @@
+# iOS-Study
+
+## 2 주차 스터디
+
+- Auto Layout 작성.
